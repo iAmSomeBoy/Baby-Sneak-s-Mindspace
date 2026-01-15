48ca58629bcf861748bf107c236b721b521ec00b
iAmSomeBoy <ashikur2401@gmail.com>
2026-01-15 20:22:50 +0600

    Add initial scripts for coin flip, grade evaluation, and pH level assessment

---
+++ messy_codes/grades.py
@@ -0,0 +1,5 @@
+grade= 60
+if grade>= 55:   # bracket is needed like c or c++ syntax
+  print('You passed')
+else:
+  print('You failed')
---
+++ messy_codes/ph_levels.py
@@ -0,0 +1,7 @@
+ph = 10
+if ph > 7 :
+  print("Basic")
+elif ph < 7 :
+  print("Acidic")
+else:
+  print("Neutral")
